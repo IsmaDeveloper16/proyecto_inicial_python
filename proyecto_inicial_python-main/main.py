ac0471ae4a70b99c0cbaeb50db27fc3622cfde66
IsmaDeveloper16 <117777526+IsmaDeveloper16@users.noreply.github.com>
2023-01-06 23:32:41 -0300

    Add files via upload

---
+++ proyecto_inicial_python-main/main.py
@@ -0,0 +1,81 @@
+import csv
+import random
+import interfaz
+
+def leer_palabra_secreta(lista):
+    with open(lista, "r") as csvfile:
+        palabras = list(csv.DictReader(csvfile))
+    secret = random.choice(palabras)
+    palabra_secreta = secret["palabras"]
+    return palabra_secreta
+
+def pedir_letra(letras_usadas):
+    while True:
+        letra = str(input("ingrese una letra: ")).lower()
+        if len(letra) == 1 and letra.isalpha() == True:
+            if letra in letras_usadas:
+                pass
+            else:
+                letras_usadas.append(letra)
+                break
+        else:
+            pass
+    return letra
+
+def verificar_letra(letras, palabra_secreta):
+    if letras in palabra_secreta:
+        return True
+    else:
+        return False
+
+def validar_palabra(letras_usadas, palabra_secreta):
+    while True:
+        for letra_secreta in palabra_secreta:
+            if letra_secreta in letras_usadas:
+                pass
+            else:
+                print("aun no se adivino la palabra")
+                return False
+                break
+        return True
+        break
+
+
+if __name__ == "__main__":
+    print("\n¡Aquí comienza el juego del ahorcado!\n")
+    # Inicializo las variables y listas a utilizar.
+    max_cantidad_intentos = 7
+    intentos = 0
+    letras_usadas = []
+    es_ganador = False
+
+    # Leer la palabra secreta de un archivo csv.
+    palabra_secreta = leer_palabra_secreta('palabras.csv')
+    
+    # Esto se realiza para que el jugador pueda ver al principio
+    # la cantidad de letras de la palabra a adivinar.
+    interfaz.dibujar(palabra_secreta, letras_usadas, intentos)
+    
+    while intentos < max_cantidad_intentos == 7 and not es_ganador:
+        # Pedir una nueva letra
+        letra = pedir_letra(letras_usadas)
+
+        # Verificar si la letra es parte de la palabra secreta        
+        if verificar_letra(letra, palabra_secreta) == False:
+            # En caso de no estar la letra ingresada en la palabra
+            # a adivinar incremento en 1 la variable intentos.
+            intentos += 1
+        
+        # Dibujar la interfaz
+        interfaz.dibujar(palabra_secreta, letras_usadas, intentos)
+
+        # Validar si la palabra secreta se ha adivinado
+        if validar_palabra(letras_usadas, palabra_secreta) == True:
+            es_ganador = True
+            break
+
+    if es_ganador:
+        print(f'\n¡Usted ha ganado la partida!, palabra secreta {palabra_secreta}!\n')
+    else:
+        print('\n¡Ahorcado!')
+        print(f'\n¡Usted ha perdido la partida!, palabra secreta {palabra_secreta}!\n')
